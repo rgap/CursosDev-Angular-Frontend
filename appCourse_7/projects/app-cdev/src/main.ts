eb5d3ebf8d20d844d7dcf6e4d5298710163b46e5
rgap <r.guzmanap@gmail.com>
2024-05-26 10:00:19 -0500

    pagina y componentes

--- appCourse_7/projects/app-cdev/src/main.ts
+++ appCourse_7/projects/app-cdev/src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { appConfig } from './app/app.config';
 import { UsernameComponent } from './app/username.component';
+
+// platformBrowserDynamic().bootstrapModule(AppModule);
 
 bootstrapApplication(UsernameComponent, appConfig).catch((err) =>
   console.error(err)
